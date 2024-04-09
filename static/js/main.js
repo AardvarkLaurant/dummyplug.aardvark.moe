9dc3ff039d598778b2a3590148ba67267246270a
AardvarkLaurant <scpinkyblt@gmail.com>
2024-04-09 17:52:41 -0400

    Fixing

--- static/js/main.js
+++ static/js/main.js
@@ -34,7 +34,7 @@
             $(`#${e.target.getAttribute('aria-controls')}`).classList.toggle('show');
             e.target.classList.toggle('active');
 
-        $$('#pane-right > .show') ? reader.classList.add('two-pane') : reader.classList.remove('two-pane');
+            $$('.pane-item.show') ? reader.classList.add('two-pane') : reader.classList.remove('two-pane');
         });
     });
 
